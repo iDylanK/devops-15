993015c3ac442f9684b2338b35afcf6eb6a4e282
Vasileios Christou <christou_vasileios@hotmail.com>
2022-03-29 19:03:02 +0200

    desc text added

--- app/movieguessr/tests/tests_basic.py
+++ app/movieguessr/tests/tests_basic.py
@@ -1,11 +1,14 @@
+''' Basic Tests '''
+
 from django.test import TestCase
 
 
 class BasicTestCase(TestCase):
-
+    ''' Basic Case '''
     def setUp(self):
-        pass
+        ''' Set Up '''
 
     def test_home_access(self):
+        ''' Test '''
         response = self.client.get("/", follow=True)
         self.assertEqual(response.status_code, 200)
